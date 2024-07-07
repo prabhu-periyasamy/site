910a58d34b3837d49f5112a8f55e0487b27e3edb
prabhu periyasamy <prabhu.periyasamy@freshworks.com>
2024-07-07 22:27:14 +0530

    next

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,9 @@
 
 9
 	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+A
+simple/index.html,c/c/cceaa2b83a781c501331973bc8636e34148bee51
+@
+simple/style.css,5/b/5bb5e6b46b2b66b96092697e0715a62b2f9bc49e
+@
+simple/submit.js,c/e/ce07bc3968238ef55388eed86a064d780d27150b
--- simple/submit.js
+++ simple/submit.js
@@ -1,7 +1,11 @@
-function submitForm() {
-    var aa = document.getElementById("form-section");
+function showBasicForm() {
+    document.getElementById("basic-form").style.display = "block";
+    document.getElementById("all-form").style.display = "none";
+    document.querySelector(".form-success").style.display = "none"; // Hide success message on form switch
+}
 
-    setTimeout(() => {
-        aa.classList.toggle("toogle-success");
-    }, 3000);
+function showAllForm() {
+    document.getElementById("basic-form").style.display = "none";
+    document.getElementById("all-form").style.display = "block";
+    document.querySelector(".form-success").style.display = "none"; // Hide success message on form switch
 }
